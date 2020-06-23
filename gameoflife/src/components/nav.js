83194e3caf410a2240e36928c2a90b261f0f4b43
BrandyBecker <brandybeckerfsw@gmail.com>
2020-06-23 15:15:41 -0400

    restarted react app, includes grid and cells

---
+++ gameoflife/src/components/nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>John Conway's</h1>
+      <h1 class="title">👾Game of Life👾</h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
