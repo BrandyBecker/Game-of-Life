1f5fbe2998baf6e0b5203f4444a1c5fa5fc946c8
BrandyBecker <brandybeckerfsw@gmail.com>
2020-06-23 17:14:17 -0400

    adds footer and links to github

---
+++ gameoflife/src/components/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+      <a href='https://www.github.com/brandybecker' target="_blank" className="footer-link">Project © Brandy Becker 2020</a>
+      </div>
+    );
+  }
+}
+
+export default Footer;
