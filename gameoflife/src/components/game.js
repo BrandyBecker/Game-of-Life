0575159e1b9393e4f7128c2f7d3682376bb681ea
BrandyBecker <brandybeckerfsw@gmail.com>
2020-06-25 10:51:54 -0400

    Formats code to be more readable

--- gameoflife/src/components/game.js
+++ gameoflife/src/components/game.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./game.css";
 import GameCell from './gamecell'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const game_cell = 20; // cell size
 const game_width = 640; // grid width
@@ -9,139 +9,138 @@ const game_height = 500; // grid height
 
 
 class Game extends React.Component {
-    constructor() {
-        super();
-        this.rows = game_height / game_cell; //rows = height / cell
-        this.columns = game_width / game_cell; //cols = width / cell
-        this.gameboard = this.makeNewBoard(); // makeNewBoard function
-    }
-    state = {
-        gamecells: [], 
-        interval: 100, //speed
-        isEngaged: false, //starts dead
-        generationNum: 0 //starts at gen 0
-    };
+  constructor() {
+    super();
+    this.rows = game_height / game_cell; //rows = height / cell
+    this.columns = game_width / game_cell; //cols = width / cell
+    this.gameboard = this.makeNewBoard(); // makeNewBoard function
+  }
+  state = {
+    gamecells: [],
+    interval: 100, //speed
+    isEngaged: false, //starts dead
+    generationNum: 0 //starts at gen 0
+  };
 
-    makeNewBoard() { //new board
+  makeNewBoard() { //new board
     let gameboard = [];
     for (let y = 0; y < this.rows; y++) {
-        gameboard[y] = [];
-            for (let x = 0; x < this.columns; x++) {
-                gameboard[y][x] = false; 
-            }
-        }
-        return gameboard;
+      gameboard[y] = [];
+      for (let x = 0; x < this.columns; x++) {
+        gameboard[y][x] = false;
+      }
     }
-     
+    return gameboard;
+  }
 
-    makeNewCells() { //new cells
+  makeNewCells() { //new cells
     let gamecells = [];
     for (let y = 0; y < this.rows; y++) {
-        for (let x = 0; x < this.columns; x++) {
+      for (let x = 0; x < this.columns; x++) {
         if (this.gameboard[y][x]) {
-            gamecells.push({ x, y });
+          gamecells.push({ x, y });
         }
+      }
     }
-    }
-        return gamecells;
-    }
-    
-    getElement() {
+    return gamecells;
+  }
+
+  getElement() {
     const rect = this.boardRef.getBoundingClientRect();
     const doc = document.documentElement;
-    
-        return {
-            x: rect.left + window.pageXOffset - doc.clientLeft,
-            y: rect.top + window.pageYOffset - doc.clientTop
-        };
-    }
-    
-    handleClick = e => {
+
+    return {
+      x: rect.left + window.pageXOffset - doc.clientLeft,
+      y: rect.top + window.pageYOffset - doc.clientTop
+    };
+  }
+
+  handleClick = e => {
     const elemOffset = this.getElement();
     const offsetX = e.clientX - elemOffset.x;
     const offsetY = e.clientY - elemOffset.y;
-  
+
     const x = Math.floor(offsetX / game_cell);
     const y = Math.floor(offsetY / game_cell);
-  
+
     if (x >= 0 && x <= this.columns && y >= 0 && y <= this.rows) {
-        this.gameboard[y][x] = !this.gameboard[y][x];
+      this.gameboard[y][x] = !this.gameboard[y][x];
     }
-        this.setState({ gamecells: this.makeNewCells() });
-    };
-    
-    startGame = () => { //starts game
-        this.setState({ isEngaged: true }, () => this.iterationMethod());
-    };
-    
-    stopGame = () => { //stops game
-        this.setState({ isEngaged: false });
-        if (this.timeoutHandler) {
-            window.clearTimeout(this.timeoutHandler);
-            this.timeoutHandler = null;
-        }
-    };
+    this.setState({ gamecells: this.makeNewCells() });
+  };
 
-    handleClear = () => { //clears & resets gen
-        this.gameboard = this.makeNewBoard();
-        this.setState({ gamecells: this.makeNewCells() , isEngaged: false , generationNum: -1 }); //if game in progress, stops & sets gen to 0
-      };
+  startGame = () => { //starts game
+    this.setState({ isEngaged: true }, () => this.iterationMethod());
+  };
 
-      handleClear2 = () => { //clears & resets gen
-        this.gameboard = this.makeNewBoard();
-        this.setState({ gamecells: this.makeNewCells() , isEngaged: false , generationNum: 0 }); // if game not in progress / stopped, sets to 0
-      };
+  stopGame = () => { //stops game
+    this.setState({ isEngaged: false });
+    if (this.timeoutHandler) {
+      window.clearTimeout(this.timeoutHandler);
+      this.timeoutHandler = null;
+    }
+  };
 
-      handleRandom = () => {
-        for (let y = 0; y < this.rows; y++) {
-          for (let x = 0; x < this.columns; x++) {
-            this.gameboard[y][x] = Math.random() >= 0.7;
+  handleClear = () => { //clears & resets gen
+    this.gameboard = this.makeNewBoard();
+    this.setState({ gamecells: this.makeNewCells(), isEngaged: false, generationNum: -1 }); //if game in progress, stops & sets gen to 0
+  };
+
+  handleClear2 = () => { //clears & resets gen
+    this.gameboard = this.makeNewBoard();
+    this.setState({ gamecells: this.makeNewCells(), isEngaged: false, generationNum: 0 }); // if game not in progress / stopped, sets to 0
+  };
+
+  handleRandom = () => {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.columns; x++) {
+        this.gameboard[y][x] = Math.random() >= 0.7;
+      }
+    }
+    this.setState({ gamecells: this.makeNewCells() });
+  };
+
+  iterationMethod = () => {
+    let newGameBoard = this.makeNewBoard();
+
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.columns; x++) {
+        let neighbors = this.neighborsMethod(this.gameboard, x, y);
+        if (this.gameboard[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newGameBoard[y][x] = true;
+          }
+          else {
+            newGameBoard[y][x] = false;
           }
         }
-        this.setState({ gamecells: this.makeNewCells() });
-      };
-
-    iterationMethod = () => {
-    let newGameBoard = this.makeNewBoard();
-    
-    for (let y = 0; y < this.rows; y++) {
-        for (let x = 0; x < this.columns; x++) {
-        let neighbors = this.neighborsMethod(this.gameboard, x, y);
-            if (this.gameboard[y][x]) {
-                if (neighbors === 2 || neighbors === 3) {
-                    newGameBoard[y][x] = true;
-                } 
-                else {
-                    newGameBoard[y][x] = false;
-                }
-            } 
-            else {
-                if (!this.gameboard[y][x] && neighbors === 3) {
-                    newGameBoard[y][x] = true;
-            }
+        else {
+          if (!this.gameboard[y][x] && neighbors === 3) {
+            newGameBoard[y][x] = true;
+          }
         }
-        }
+      }
     }
 
     this.gameboard = newGameBoard;
     this.setState({ gamecells: this.makeNewCells() });
 
     if (this.state.isEngaged) {
-    this.timeoutHandler = window.setTimeout(() => {
+      this.timeoutHandler = window.setTimeout(() => {
         this.iterationMethod();
-    }, this.state.interval);
+      }, this.state.interval);
     }
 
     let newGeneration = this.state.generationNum;
     ++newGeneration;
     this.setState({ generationNum: newGeneration });
     console.log(this.state.generationNum);
-    }
+  }
 
   neighborsMethod(gameboard, x, y) {
     let neighbors = 0;
-    const dirs = [[-1, -1],[-1, 0],[-1, 1],[0, 1],[1, 1],[1, 0],[1, -1],[0, -1]]; //8 neighbors in all directions
-    for (let i = 0; i < dirs.length; i++) { 
+    const dirs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]; //8 neighbors in all directions
+    for (let i = 0; i < dirs.length; i++) {
       const dir = dirs[i];
       let y1 = y + dir[0];
       let x1 = x + dir[1];
@@ -156,7 +155,7 @@ class Game extends React.Component {
         neighbors++;
       }
     }
-    return neighbors; 
+    return neighbors;
   }
 
   handleInterval = e => {
@@ -168,70 +167,70 @@ class Game extends React.Component {
     return (
       <div class="flex">
         <div className="controls">
-        <div ><h3 class="gen">Generation: {this.state.generationNum}</h3>
-          {this.state.isEngaged ? (
-            <button className="button" onClick={this.stopGame}> 
-              Stop Game
-            </button> //stops game
-          ) : (
-            <button className="button" onClick={this.startGame}>
-              Start Game
-            </button> //starts game
-          )}
-          <button className="button" onClick={this.handleRandom}>
-            Random
-          </button> 
-          {/* randomizes cells on board */}
-          <button className="button" onClick={this.iterationMethod}>
-            One Step
+          <div ><h3 class="gen">Generation: {this.state.generationNum}</h3>
+            {this.state.isEngaged ? (
+              <button className="button" onClick={this.stopGame}>
+                Stop Game
+              </button> //stops game
+            ) : (
+                <button className="button" onClick={this.startGame}>
+                  Start Game
+                </button> //starts game
+              )}
+            <button className="button" onClick={this.handleRandom}>
+              Random
           </button>
-          {/* 1 generation at a time */}
-          {this.state.isEngaged ? (
-            <button className="button" onClick={this.handleClear}>
-            Clear
-          </button> ) : (
-          <button className="button" onClick={this.handleClear2}>
-          Clear
-        </button> 
-        // If game is engaged, clears board and resets generations
-        // If game is not engaged, resets generations to 0
-          )}
+            {/* randomizes cells on board */}
+            <button className="button" onClick={this.iterationMethod}>
+              One Step
+          </button>
+            {/* 1 generation at a time */}
+            {this.state.isEngaged ? (
+              <button className="button" onClick={this.handleClear}>
+                Clear
+              </button>) : (
+                <button className="button" onClick={this.handleClear2}>
+                  Clear
+                </button>
+                // If game is engaged, clears board and resets generations
+                // If game is not engaged, resets generations to 0
+              )}
 
-        <div className="speed">
-          Update Every{" "}
-          <input
-            value={this.state.interval}
-            onChange={this.handleInterval}
-          />{" "}
+            <div className="speed">
+              Update Every{" "}
+              <input
+                value={this.state.interval}
+                onChange={this.handleInterval}
+              />{" "}
           milliseconds
           </div>
-        </div>
-        <div className="container">
-        <div
-          className="gameBoard"
-          style={{
-            width: game_width,
-            height: game_height,
-            backgroundSize: `${game_cell}px ${game_cell}px`
-          }}
-          onClick={this.handleClick}
-          ref={n => {
-            this.boardRef = n;
-          }}
-        >
-          {gamecells.map(gamecell => (
-            <GameCell x={gamecell.x} y={gamecell.y} key={`${gamecell.x}, ${gamecell.y}`} />
-          ))}
-        </div>
-          
-          <div>
-            <div> <Link to='/' className="home-link"><button className="home-button button">👈Go Home👈</button></Link></div>
-          </div> 
-          
           </div>
-          
+          <div className="container">
+            <div
+              className="gameBoard"
+              style={{
+                width: game_width,
+                height: game_height,
+                backgroundSize: `${game_cell}px ${game_cell}px`
+              }}
+              onClick={this.handleClick}
+              ref={n => {
+                this.boardRef = n;
+              }}
+            >
+              {gamecells.map(gamecell => (
+                <GameCell x={gamecell.x} y={gamecell.y} key={`${gamecell.x}, ${gamecell.y}`} />
+              ))}
+            </div>
+
+            <div>
+              <div> <Link to='/' className="home-link"><button className="home-button button">👈Go Home👈</button></Link></div>
+            </div>
+
+          </div>
+
         </div>
-        
+
       </div>
     );
   }
--- gameoflife/src/components/home.js
+++ gameoflife/src/components/home.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "./home.css";
 
 class Home extends React.Component {
-    render() {
-      return (
-        <div className="Homepage">
- 
+  render() {
+    return (
+      <div className="Homepage">
+
         <div className="hompage-container">
-        <div className="go-play">
-          <div className="play-header"><h2>⬇ Play Now! ⬇</h2></div>
+          <div className="go-play">
+            <div className="play-header"><h2>⬇ Play Now! ⬇</h2></div>
             <Link exact to='/game' className="game-link"><button className="play-button button"> 🎮 Play Game! 🎮</button></Link>
           </div>
         </div>
-        <div className="rules"> 
-            <h3>Rules of the Game:</h3>
-                <p>1. Any live cell with fewer than 2 live neighbors dies, as if by underpopulation.</p>
-                <p>2. Any live cell with 2 or 3 live neighbors lives on to the next generation.</p>
-                <p>3. Any live cell with more than 3 live neighbors dies, as if by overpopulation.</p>
-                <p>4. Any dead cell with exactly 3 live neighbors becomes a live cell, as if by reproduction.</p>
-            </div>
-        <div className="about">
-            <h3>About:</h3>
-            <p>The Game of Life is a <a class="wikilinks" href="https://en.wikipedia.org/wiki/Cellular_automaton">cellular automation</a> created by <a class="wikilinks" href="https://en.wikipedia.org/wiki/John_Horton_Conway">John Horton Conway</a> in 1970. Although it is called a game, it actually has zero players. The player only participates in setting the initial state, and the evolution of the patterns begins moving forward. The general setup is a grid with cells showing as 'alive' or 'dead'.</p>
-            <a class="gameoflife" href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">📔 Wiki 📔</a>
-            </div>
+        <div className="rules">
+          <h3>Rules of the Game:</h3>
+          <p>1. Any live cell with fewer than 2 live neighbors dies, as if by underpopulation.</p>
+          <p>2. Any live cell with 2 or 3 live neighbors lives on to the next generation.</p>
+          <p>3. Any live cell with more than 3 live neighbors dies, as if by overpopulation.</p>
+          <p>4. Any dead cell with exactly 3 live neighbors becomes a live cell, as if by reproduction.</p>
         </div>
+        <div className="about">
+          <h3>About:</h3>
+          <p>The Game of Life is a <a class="wikilinks" href="https://en.wikipedia.org/wiki/Cellular_automaton">cellular automation</a> created by <a class="wikilinks" href="https://en.wikipedia.org/wiki/John_Horton_Conway">John Horton Conway</a> in 1970. Although it is called a game, it actually has zero players. The player only participates in setting the initial state, and the evolution of the patterns begins moving forward. The general setup is a grid with cells showing as 'alive' or 'dead'.</p>
+          <a class="gameoflife" href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">📔 Wiki 📔</a>
+        </div>
+      </div>
 
-      );
-    }
+    );
   }
-  
-  export default Home;
+}
+
+export default Home;
